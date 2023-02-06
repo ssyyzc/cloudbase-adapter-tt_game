632a1b9d16b70caa2572d127b0066a25f8aa4850
ssyyzc_001 <ssyyzc_001@163.com>
2023-02-06 18:44:23 +0800

    正式

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,81 @@
 import { TTMpWebSocket, ttMpStorage, TTRequest } from '@maoyan/cloudbase-adapter-tt_mp';
 import { SDKAdapterInterface, WebSocketContructor, StorageType } from '@cloudbase/adapter-interface';
 
+// eslint-disable-next-line
 declare const tt;
 declare const GameGlobal;
 
-function isMatch() {
-    if (typeof tt === 'undefined') {
-        return false;
+function isMatch(): boolean {
+  if (typeof tt === 'undefined') {
+    return false;
+  }
+  if (typeof GameGlobal === 'undefined') {
+    return false;
+  }
+  if (!tt.onHide) {
+    return false;
+  }
+  if (!tt.offHide) {
+    return false;
+  }
+  if (!tt.onShow) {
+    return false;
+  }
+  if (!tt.offShow) {
+    return false;
+  }
+  if (!tt.getSystemInfoSync) {
+    return false;
+  }
+  if (!tt.getStorageSync) {
+    return false;
+  }
+  if (!tt.setStorageSync) {
+    return false;
+  }
+  if (!tt.connectSocket) {
+    return false;
+  }
+  if (!tt.request) {
+    return false;
+  }
+
+  try {
+    if (!tt.getSystemInfoSync()) {
+      return false;
     }
-    if (typeof GameGlobal === 'undefined') {
-        return false;
-    }
-    if (!tt.onHide) {
-        return false;
-    }
-    if (!tt.offHide) {
-        return false;
-    }
-    if (!tt.onShow) {
-        return false;
-    }
-    if (!tt.offShow) {
-        return false;
-    }
-    if (!tt.getSystemInfoSync) {
-        return false;
-    }
-    if (!tt.getStorageSync) {
-        return false;
-    }
-    if (!tt.setStorageSync) {
-        return false;
-    }
-    if (!tt.connectSocket) {
-        return false;
-    }
-    if (!tt.request) {
-        return false;
-    }
-    try {
-        if (!tt.getSystemInfoSync()) {
-            return false;
-        }
-    }
-    catch (e) {
-        return false;
-    }
-    return true;
+  } catch (e) {
+    return false;
+  }
+
+  return true;
 }
+
 function genAdapter() {
-    var adapter: SDKAdapterInterface = {
-        root: GameGlobal,
-        reqClass: TTRequest,
-        wsClass: TTMpWebSocket as WebSocketContructor,
-        localStorage: ttMpStorage,
-        primaryStorage: StorageType.local,
-        getAppSign: function () {
-            return '';
-        }
-    };
-    return adapter;
+  // 小程序无sessionStorage
+  const adapter: SDKAdapterInterface = {
+    root: GameGlobal,
+    reqClass: TTRequest,
+    wsClass: TTMpWebSocket as WebSocketContructor,
+    localStorage: ttMpStorage,
+    primaryStorage: StorageType.local,
+    getAppSign(){
+      return  '';
+    }
+  };
+  return adapter;
 }
-var adapter = {
-    genAdapter: genAdapter,
-    isMatch: isMatch,
-    runtime: 'tt_game'
+
+const adapter = { 
+  genAdapter, 
+  isMatch, 
+  runtime:'tt_game' 
 };
-try {
-    window['adapter'] = adapter;
-    GameGlobal.adapter = adapter;
-}
-catch (e) { }
-export { adapter };
+
+try{
+  window['adapter'] = adapter;
+  GameGlobal.adapter = adapter;
+}catch(e){}
+
+export {adapter};
 export default adapter;
